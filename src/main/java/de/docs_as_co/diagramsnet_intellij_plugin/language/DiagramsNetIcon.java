4553ab4d49e9e7b8bb2a7707f9a48120d109b984
Henning Dieterichs <henning.dieterichs@jetbrains.com>
2020-10-12 15:42:04 +0200

    Removes existing content.

--- src/main/java/de/docs_as_co/diagramsnet_intellij_plugin/language/DiagramsNetIcon.java
+++
@@ -1,9 +0,0 @@
-package de.docs_as_co.diagramsnet_intellij_plugin.language;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public class DiagramsNetIcon {
-    public static final Icon FILE = IconLoader.getIcon("/icons/diagrams.net.16.png");
-}
